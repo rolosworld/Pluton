cf3b4ce77c5f154e578c0c5cce4adf5965da4cc4
Rolando Gonzalez-Chevere <rolosworld@gmail.com>
2018-01-19 22:39:17 -0500

    - Add more functionality to pluton_db_recreate.pl

--- bin/pluton_db_recreate.pl
+++ bin/pluton_db_recreate.pl
@@ -4,16 +4,52 @@ use strict;
 use warnings;
 
 my $num_args = $#ARGV + 1;
-if ( $num_args != 5 ) {
-    print "Usage: pluton_db_recreate.pl <hostname> <port> <database> <user> <pass>\n";
-    exit;
+my $script_mode = 0;
+
+my $host;
+my $port;
+my $database;
+my $user;
+my $pass;
+my $postgre_pass;
+
+for ( my $i = 0; $i < scalar @ARGV; $i++ ) {
+    my $arg = $ARGV[$i];
+    if ( $arg eq '-n' ) {
+        $script_mode = 1;
+    }
+    else {
+        my @parts = split( '=', $arg );
+        if ( scalar @parts < 2) {
+            print "Usage: pluton_db_recreate.pl [-n] hostname=<hostname> port=<port> database=<database> user=<user> pass=<pass> postgre_pass=<postgre_pass>\n";
+            exit;
+        }
+
+        if ( $parts[0] eq 'hostname' ) {
+            $host = $parts[1];
+        }
+        elsif ( $parts[0] eq 'port' ) {
+            $port = $parts[1];
+        }
+        elsif ( $parts[0] eq 'database' ) {
+            $database = $parts[1];
+        }
+        elsif ( $parts[0] eq 'user' ) {
+            $user = $parts[1];
+        }
+        elsif ( $parts[0] eq 'pass' ) {
+            $pass = $parts[1];
+        }
+        elsif ( $parts[0] eq 'postgre_pass' ) {
+            $postgre_pass = $parts[1];
+        }
+    }
 }
 
-my $host = $ARGV[0];
-my $port = $ARGV[1];
-my $database = $ARGV[2];
-my $user = $ARGV[3];
-my $pass = $ARGV[4];
+if ( !($host && $port && $database && $user && $pass && $postgre_pass) ) {
+    print "Usage: pluton_db_recreate.pl [-n] hostname=<hostname> port=<port> database=<database> user=<user> pass=<pass> postgre_pass=<postgre_pass>\n";
+    exit;
+}
 
 my $filename = 'conf/pluton_local.pl';
 open(my $fh, '<:encoding(UTF-8)', $filename)
@@ -41,9 +77,11 @@ open($fh, '>:encoding(UTF-8)', $filename)
 
 print $fh join( "\n", @output );
 
-print(`echo "*:*:$database:$user:$pass" > ~/.pgpass`);
-print(`dropdb -h $host -p $port -U $user $database`);
-print(`createdb -E UNICODE -h $host -p $port -U $user $database`);
-print(`psql -h $host -p $port -U $user $database < sql/main.sql`);
-print(`psql -h $host -p $port -U $user $database < sql/websockets.sql`);
-print(`psql -h $host -p $port -U $user $database < sql/pluton.sql`);
+if ( !$script_mode ) {
+    print(`PGPASSWORD=$postgre_pass psql -h $host -U postgres -c "CREATE USER $user WITH CREATEDB ENCRYPTED PASSWORD '"$pass"';"`);
+    print(`PGPASSWORD=$pass dropdb -h $host -p $port -U $user $database`);
+    print(`PGPASSWORD=$pass createdb -E UNICODE -h $host -p $port -U $user $database`);
+    print(`PGPASSWORD=$pass psql -h $host -p $port -U $user $database < sql/main.sql`);
+    print(`PGPASSWORD=$pass psql -h $host -p $port -U $user $database < sql/websockets.sql`);
+    print(`PGPASSWORD=$pass psql -h $host -p $port -U $user $database < sql/pluton.sql`);
+}
