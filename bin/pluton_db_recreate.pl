7ce47c15ef8af4bc65a11e62b1d0c035863ccd2f
Rolando Gonzalez-Chevere <rolosworld@gmail.com>
2018-01-19 19:21:06 -0500

    - Add port - Add -U option into the commands

--- bin/pluton_db_recreate.pl
+++ bin/pluton_db_recreate.pl
@@ -5,12 +5,13 @@ use warnings;
 
 my $num_args = $#ARGV + 1;
 if ( $num_args != 4 ) {
-    print "Usage: pluton_db_recreate.pl <database> <hostname> <user> <pass>\n";
+    print "Usage: pluton_db_recreate.pl <hostname> <port> <database> <user> <pass>\n";
     exit;
 }
 
-my $database = $ARGV[0];
-my $host = $ARGV[1];
+my $host = $ARGV[0];
+my $port = $ARGV[1];
+my $database = $ARGV[1];
 my $user = $ARGV[2];
 my $pass = $ARGV[3];
 
@@ -23,7 +24,7 @@ while (my $row = <$fh>) {
     chomp $row;
 
     if ( $row =~ / dsn => .+/ ) {
-        $row =~ s/ dsn => .+/ dsn => 'dbi:Pg:dbname=$database;host=$host',/;
+        $row =~ s/ dsn => .+/ dsn => 'dbi:Pg:dbname=$database;host=$host;port=$port',/;
     }
     elsif ( $row =~ / user => .+/ ) {
         $row =~ s/ user => .+/ user => '$user',/; 
@@ -40,8 +41,8 @@ open($fh, '>:encoding(UTF-8)', $filename)
 
 print $fh join( "\n", @output );
 
-print(`dropdb -h $host $database`);
-print(`createdb -E UNICODE $database`);
-print(`psql -h $host $database < sql/main.sql`);
-print(`psql -h $host $database < sql/websockets.sql`);
-print(`psql -h $host $database < sql/pluton.sql`);
+print(`dropdb -h $host -p $port -U $user $database`);
+print(`createdb -E UNICODE -h $host -p $port -U $user $database`);
+print(`psql -h $host -p $port -U $user $database < sql/main.sql`);
+print(`psql -h $host -p $port -U $user $database < sql/websockets.sql`);
+print(`psql -h $host -p $port -U $user $database < sql/pluton.sql`);
